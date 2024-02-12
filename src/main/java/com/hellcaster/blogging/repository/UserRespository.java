de973bd509ce574911a1916aaebd9c72a71deae7
luckyx3 <lucky.singh.12092000@gmail.com>
2024-02-12 16:33:35 +0530

    Test case Added

--- src/main/java/com/hellcaster/blogging/repository/UserRespository.java
+++ src/main/java/com/hellcaster/blogging/repository/UserRespository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
-//    User findByUserNameAndPassword(String userName, String password);
+public interface AppUserRespository extends MongoRepository<User, String> {
+    User findByUserNameAndPassword(String userName, String password);
+    User findByUserName(String userName);
 }
